edb92982afea692b15097fa9bc5e4b919550c0e0
FriendlyFire <jpguertin.cg@gmail.com>
2020-04-25 00:27:10 -0400

    Montage du dossier partagé, exécution locale.

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if ["$#" -ne 1 ]; then
+    echo "Please provide the SAMBA share path as an argument (no spaces in path)."
+fi
 
 # Python
 sudo apt install python3
@@ -6,12 +12,15 @@ pip3 install -r requirements.txt
 
 # Crontab setup
 crontab -l > cron.tmp
-echo "* * * * * python3 ~/share/Cadre/cadre.py > ~/share/Cadre/cadre.log 2>&1" >> cron.tmp
-echo "* * * * * python3 ~/share/Cadre/cadre.py > ~/share/Cadre/cadre.log 2>&1" >> cron.tmp
+echo "* * * * * python3 $DIR/cadre.py > $DIR/cadre.log 2>&1" >> cron.tmp
+echo "* * * * * python3 $DIR/cadre.py > $DIR/cadre.log 2>&1" >> cron.tmp
 echo "30 23 * * * xset dpms force off" >> cron.tmp
 echo "0 8 * * * xset dpms force on" >> cron.tmp
 crontab cron.tmp
 rm cron.tmp
+
+sudo mkdir /mnt/photos
+sudo echo "$1 /mnt/photos cifs _netdev,username=root,password=,dir_mode=0755,uid=500,gid=500 0 0" >> /etc/fstab
 
 sudo mount -o rw,remount /boot
 sudo echo "hdmi_blanking=1" >> /boot/config.txt
