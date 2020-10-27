9036f016a0d1424bbeec064b2ef50efd3b653425
FriendlyFire <jpguertin.cg@gmail.com>
2020-10-26 23:44:15 -0400

    Some more safety checks.

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+if grep -q '\\' "$1" ; then
+    echo "Please use /'s rather than \\'s in your SAMBA share path."
+    exit 1
+fi
+
 # Python
 sudo apt -y install python3 python3-pip cifs-utils freeglut3-dev unclutter xscreensaver
 sudo pip3 install -r requirements.txt
@@ -18,4 +23,8 @@ crontab cron.tmp
 rm cron.tmp
 
 sudo mkdir -p /mnt/photos
-sudo su -c "echo '$1 /mnt/photos   ro,guest,mode=0555,noexec,user=nobody 0 0' >> /etc/fstab"
+if ! grep -q $1 /etc/fstab; then
+    sudo su -c "echo '$1 /mnt/photos    cifs    ro,noexec,user=nobody,guest 0 0' >> /etc/fstab"
+else
+    echo "Mount entry for $1 already exists, skipping..."
+fi
